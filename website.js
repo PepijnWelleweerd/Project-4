bc66592098c73dcdc04c6907917742b15099946e
PepijnWelleweerd <33251987+PepijnWelleweerd@users.noreply.github.com>
2018-03-08 13:40:42 +0100

    Add files via upload

---
+++ website.js
@@ -0,0 +1,78 @@
+//Javascript voor Colicss//
+
+
+	function rekentool(){
+
+			var duur = prompt("Voor welke periode zou U het pakket graag willen?", "1 maand/1 jaar/5 jaar");
+			 if (duur == "1 maand")
+			 	alert("Voor het pakket 'Cloud - Brons 1 maand' betaald U €4.");
+			 	else if (duur == "1 jaar")
+			 		alert("Voor het pakket 'Cloud - Brons 1 jaar' betaald U €48,-.");
+			 	else if (duur == "5 jaar")
+			 		alert("Voor het pakket 'Cloud - Brons 5 jaar' betaald U €240,-.");
+		}
+	function rekentool3(){
+
+			var duur = prompt("Voor welke periode zou U het pakket graag willen?", "1 maand/1 jaar/5 jaar");
+			 if (duur == "1 maand")
+			 	alert("Voor het pakket 'Cloud - Silver 1 maand' betaald U €6.");
+			 	else if (duur == "1 jaar")
+			 		alert("Voor het pakket 'Cloud - Silver 1 jaar' betaald U €72,-.");
+			 	else if (duur == "5 jaar")
+			 		alert("Voor het pakket 'Cloud - Silver 5 jaar' betaald U €360,-.");
+		}
+
+	function rekentool4(){
+		
+			var duur = prompt("Voor welke periode zou U het pakket graag willen?", "1 maand/1 jaar/5 jaar");
+			 if (duur == "1 maand")
+			 	alert("Voor het pakket 'Cloud - Goud 1 maand' betaald U €10,-.");
+			 	else if (duur == "1 jaar")
+			 		alert("Voor het pakket 'Cloud - Goud 1 jaar' betaald U €120,-.");
+			 	else if (duur == "5 jaar")
+			 		alert("Voor het pakket 'Cloud - Goud 5 jaar' betaald U €600,-.");
+		}
+
+
+	function rekentool2(){
+
+			var duur = prompt("Voor welke periode zou U het pakket graag willen?", "1 maand/1 jaar/5 jaar");
+			 if (duur == "1 maand")
+			 	alert("Voor het pakket 'Hosting - Brons 1 maand' betaald U €5,95.");
+			 	else if (duur == "1 jaar")
+			 		alert("Voor het pakket 'Hosting - Brons 1 jaar' betaald U €71,40.");
+			 	else if (duur == "5 jaar")
+			 		alert("Voor het pakket 'Hosting - Brons 5 jaar' betaald U €357,-.");
+		}
+
+	function rekentool5(){
+
+			var duur = prompt("Voor welke periode zou U het pakket graag willen?", "1 maand/1 jaar/5 jaar");
+			 if (duur == "1 maand")
+			 	alert("Voor het pakket 'Hosting - Silver 1 maand' betaald U €7,95.");
+			 	else if (duur == "1 jaar")
+			 		alert("Voor het pakket 'Hosting - Silver 1 jaar' betaald U €95,40.");
+			 	else if (duur == "5 jaar")
+			 		alert("Voor het pakket 'Hosting - Silver 5 jaar' betaald U €477,-.");
+		}
+	function rekentool6(){
+			
+			var duur = prompt("Voor welke periode zou U het pakket graag willen?", "1 maand/1 jaar/5 jaar");
+			 if (duur == "1 maand")
+			 	alert("Voor het pakket 'Hosting - Goud 1 maand' betaald U €10,95.");
+			 	else if (duur == "1 jaar")
+			 		alert("Voor het pakket 'Hosting - Goud 1 jaar' betaald U €131,40.");
+			 	else if (duur == "5 jaar")
+			 		alert("Voor het pakket 'Hosting - Goud 5 jaar' betaald U €657,-.");
+		}
+
+	function Instagram(){
+
+		alert("Helaas! Wij zijn momenteel nog niet in het bezig van een instragram pagina. Er wordt aangewerkt.")
+	}
+	
+	function fifo(){
+
+		alert("U bevind zich al op de Colicss homepagina!")
+}
+
